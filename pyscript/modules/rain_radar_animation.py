20a73297f29cd92a804d19452f134cc7d5564b67
Daniel Leong <daniel.leongjy@gmail.com>
2025-10-12 09:09:54 +0800

    Add timestamp to rain radar animation

--- pyscript/modules/rain_radar_animation.py
+++ pyscript/modules/rain_radar_animation.py
@@ -3,7 +3,7 @@ def rain_radar_animation(ms_per_frame = 500, basemap_file = '/config/www/rain_ra
     Compile rain radar snapshots into an animated GIF, ordered alphabetically by filename. ms_per_frame is the number of milliseconds to display each frame. basemap_file is the absolute path to the basemap image. input_folder is the absolute path to the folder containing the snapshots. output_folder is the absolute path to where to save the output.
     """
     import os
-    from PIL import Image
+    from PIL import Image, ImageDraw
 
     frames = [Image.open(input_folder + '/' + frame) for frame in sorted(os.listdir(input_folder))[-92:]]
     overlaid_frames = []
@@ -11,8 +11,8 @@ def rain_radar_animation(ms_per_frame = 500, basemap_file = '/config/www/rain_ra
         basemap = Image.open(basemap_file)
         basemap = basemap.resize(frame.size)
         basemap.paste(frame, (0,0), mask = frame)
-#        basemap_text = ImageDraw.Draw(basemap)
-#        basemap_text.text((0,0), frame.filename[23:31] + ' at ' + frame.filename[31:35], fill = (255,255,255))
+        basemap_text = ImageDraw.Draw(basemap)
+        basemap_text.rectangle([(395,470),(480,480)], fill = (0,0,0))
+        basemap_text.text((395,470), frame.filename[42:50] + ' at ' + frame.filename[50:54], fill = (255,255,255))
         overlaid_frames.append(basemap)
     overlaid_frames[0].save(output_folder + '/rain_radar_animation_' + frames[-1].filename[42:54] + '.gif', format = 'GIF', append_images = overlaid_frames, save_all = True, duration = 200, loop = 0)
-#    overlaid_frames[0].save(output_folder + '/rain_radar_animation.gif', format = 'GIF', append_images = overlaid_frames, save_all = True, duration = 200, loop = 0)
